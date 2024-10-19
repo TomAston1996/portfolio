d9cdd7502d7896ad4294973e83bd26fc8aa18de1
Tom Aston <tom.aston@siemens.com>
2024-10-19 19:55:14 +0100

    feat-analytics: add google tags

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,4 +1,5 @@
 import Theme from '../styles/theme';
+import Script from 'next/script'
 import Head from 'next/head'
 
 export default function App({ Component, pageProps }) {
@@ -7,6 +8,16 @@ export default function App({ Component, pageProps }) {
       <Head>
         <title>Tom Aston</title>
       </Head>
+      <Script async src="https://www.googletagmanager.com/gtag/js?id=G-BZVWYZBHVE"></Script>
+        <Script id='google-analytics'>
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+  
+            gtag('config', 'G-BZVWYZBHVE');
+          `}
+      </Script>
       <Theme>
         <Component {...pageProps} />
       </Theme>
